4deb298dd0a48dc87ffe66523c7739391e96588b
2SpaceMasterRace <alphanovachrono@gmail.com>
2023-10-17 18:41:41 +0530

    Before Error

--- part1-hello.go
+++ part1-hello.go
@@ -1,11 +1,16 @@
-package main
+ package main
 
 import "fmt"
 
+const englishHelloPrefix = "Hello "
+
 func Hello(name string) string{
-  return "Hello ," + name
+  if name == "" {
+     name = "world"
+   }
+   return englishHelloPrefix + name
 }
 
 func main(){
-  fmt.Println(Hello("world"))
+  fmt.Println(Hello("gaysus"))
 }
--- part1-hello_test.go
+++ part1-hello_test.go
@@ -3,9 +3,19 @@ package main
 import "testing"
 
 func TestHello(t *testing.T){
-  got:= Hello("world")
-  want:= "Hello ,world"
-  if got!=want{
-    t.Errorf("got %q want %q",got,want)
-  }
+  t.Run("Say Hello to other people", func(t *testing.T){ got:= Hello("Jezus")
+    want:= "Hello Jezus"
+
+    if got!=want{
+      t.Errorf("got %q, want %q",got,want)
+    }
+  })
+  t.Run("Default back to Hello world in case of empty string", func(t *testing.T){
+    got:= Hello("")
+    want:= "Hello world"
+
+    if got!=want{
+      t.Errorf("got %q, want %q",got,want)
+    }
+  })
 }
