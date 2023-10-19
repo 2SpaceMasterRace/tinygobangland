5f381b1c6dedae3befa665072b8f80c81ff8bbe7
2SpaceMasterRace <alphanovachrono@gmail.com>
2023-10-19 08:22:13 +0530

    Refactored code

--- part1-hello_test.go
+++ part1-hello_test.go
@@ -11,14 +11,14 @@ func TestHello(t *testing.T) {
 
 	t.Run("empty string defaults to 'world'", func(t *testing.T) {
 		got := Hello("")
-		want := "Hello, world"
+		want := "Hello world"
 		assertCorrectMessage(t, got, want)
 	})
 
 }
 
 func assertCorrectMessage(t testing.TB, got, want string) {
-//	t.Helper()
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
