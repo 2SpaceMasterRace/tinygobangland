08f672f6f167d841dffe11b5c9387e04a70b2d30
2SpaceMasterRace <alphanovachrono@gmail.com>
2023-10-19 13:04:10 +0530

    Refactor 1.0

--- part1-hello.go
+++ part1-hello.go
@@ -2,18 +2,22 @@ package main
 
 import "fmt"
 
-const englishHelloPrefix = "Hello "
+const spanish = "Spanish"
+const french = "French"
 
+const englishHelloPrefix = "Hello "
+const spanishHelloPrefix = "Hola "
+const frenchHelloPrefix  = "Bonjour "
 
 func Hello(name string, language string) string{
   if name == "" {
      name = "world"
    }
-  if language == "Spanish"{
-    return "Hola " + name
+  if language == spanish{
+    return spanishHelloPrefix + name
   }
-  if language == "French"{
-    return "Bonjour " + name
+  if language == french{
+    return frenchHelloPrefix + name
   }
   return englishHelloPrefix + name
 }
